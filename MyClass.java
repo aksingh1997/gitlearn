814e39bfe8e8058a01baff84bdc3bdc00ce6774e
abhishek <aksingh1997@gmail.com>
2025-01-05 16:11:05 +0530

    creating conflict

--- MyClass.java
+++ MyClass.java
@@ -1,7 +1,7 @@
 public class MyClass {
     public static void String(String[]  args) {
         System.out.println("this is a line");
-        int x = 4 + 5 + 6;
+        int x = 2 + 3 + 5 + 7;
         System.out.println("The above addition is + " + x);
     }
 }
