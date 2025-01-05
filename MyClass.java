3379e22d4a97378af8231ace9d59cff32c91acac
abhishek <aksingh1997@gmail.com>
2025-01-05 15:59:45 +0530

    added number

--- MyClass.java
+++ MyClass.java
@@ -1,7 +1,7 @@
 public class MyClass {
     public static void String(String[]  args) {
         System.out.println("this is a line");
-        int x = 4 + 5;
+        int x = 4 + 5 + 6;
         System.out.println("The above addition is + " + x);
     }
 }
