87ae9aa0815fa48e95ebbfe6f491987bb9cee5ed
Abhishek Kumar Singh <30554818+aksingh1997@users.noreply.github.com>
2025-01-05 15:40:24 +0530

    Update MyClass.java

--- MyClass.java
+++ MyClass.java
@@ -1,5 +1,6 @@
 public class MyClass {
     public static void String(String[]  args) {
+        System.out.println("this is a line");
         System.out.println("Hello, I am trying to learn git");
     }
 }
