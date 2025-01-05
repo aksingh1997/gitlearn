532cd434c4f590d2eef51cc272c90eaba740ee4f
abhishek <aksingh1997@gmail.com>
2025-01-05 15:44:28 +0530

    first commit

--- MyClass.java
+++ MyClass.java
@@ -1,6 +1,7 @@
 public class MyClass {
     public static void String(String[]  args) {
         System.out.println("this is a line");
-        System.out.println("Hello, I am trying to learn git");
+        int x = 4 + 5;
+        System.out.println("The above addition is + " + x);
     }
 }
